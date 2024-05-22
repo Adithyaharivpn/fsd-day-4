29952deaee1063f95a4e05f78ba772a79152e5d9
Adithyahari vpn <68626434+Notgood01@users.noreply.github.com>
2024-05-22 15:19:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+/*
+
+//Conditional operators
+
+if(5<10){
+    console.log("5 is less");
+}
+else{
+    console.log("10 is greater");
+}
+let age=10;
+if (age>18){
+    console.log("you are eligable for driving lisence");
+}
+else{
+    console.log("you are not eligable");
+}
+let time=23
+if (time<=12) {
+    console.log("Good Morning");
+}
+else if(time>12 && time<=15)
+    {
+    console.log("Good Afternoon");
+}
+else if (time>15 && time<=19) 
+    {
+    console.log("Good Evening");
+}
+else
+{
+    console.log("Good Night");
+}
+
+
+let a=18;
+(a<=18)?console.log("you are eligable for driving lisence"):console.log("you are not eligable for driving lisence");
+
+//
+
+for (let i=1;i<=10;i++) {
+    console.log(i);
+    
+}
+
+for (let i=1;i<=5;i++) {
+    console.log(i);
+    
+}
+
+
+let fruits=["Apple","Orange","Grapes","Banana"];
+for (let i= 0; i< fruits.length; i++) {
+    console.log(fruits[i]);   
+}
+
+
+
+let num=[1,2,3,4,5,6];
+for (let i= 0; i< num.length; i++) {
+    if (num[i]%2==0) {
+        console.log(`${num[i]} is even`);
+    } else {
+        console.log(`${num[i]} is odd`);
+    }
+}
+
+
+
+let value=[1,2,3,4,5,6,7,8,9];
+let sum=0;
+for (let i=0;i<value.length;i++) {
+    sum=sum+value[i]
+}
+console.log(sum);
+
+
+let a=1;
+let b=1;
+for (let i = 1; i <=5 ;i++) 
+    {
+    for (let j = 1; j <=10;j++)
+        {
+            console.log(`${i}*${j}=${i*j}`);
+    }
+
+    }
+
+*/
+
+//reverse
+
+const str= "Hello Nirmala!"
+for (let  i=0;  i<str.length; i++) {
+    
+}
+
